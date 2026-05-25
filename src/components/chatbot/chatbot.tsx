825420fcc95a4b6ab6dee0513adb0864bac5336b
dapperdile <rafinhawc@hotmail.com>
2026-05-25 15:44:53 -0300

    Correção link chatbot

--- src/components/chatbot/chatbot.tsx
+++ src/components/chatbot/chatbot.tsx
@@ -28,7 +28,8 @@ export function Chatbot() {
     setCarregando(true);
 
     try {
-      const response = await fetch("http://localhost:4000/chatbot/mensagem", {
+      const response = await fetch(
+    `${import.meta.env.VITE_API_URL}/chatbot/mensagem`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
